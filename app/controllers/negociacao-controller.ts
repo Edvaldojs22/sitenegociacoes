48857568992ff07c0c94f455781c58cc08947d19
Edvaldojs22 <124460629+Edvaldojs22@users.noreply.github.com>
2023-05-24 16:51:54 -0300

    final part1

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -15,6 +15,7 @@ export class NegociacaoController {
 
     adiciona(): void {
         const negociacao = this.criaNegociacao();
+        negociacao.data.setDate(15);
         this.negociacoes.adiciona(negociacao);
         console.log(this.negociacoes.lista());
         this.limparFormulario();
